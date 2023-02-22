c1b76ae540644a1b5c891c02510bf7dfc0785dd9
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2023-02-22 20:47:34 -0300

    Add files via upload

---
+++ me.vitor.dalvi.exercice.9/src/application/Program.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+    public static void main(String args[]) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Coloque as informações do cliente:");
+        System.out.print("Nome: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.next();
+        System.out.print("Data de nascimento (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Digite as informações do pedido:");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.next());
+
+        Order order = new Order(new Date(), status, client);
+
+        System.out.print("Quantos itens terão nesse pedido? ");
+        int N = sc.nextInt();
+        for (int i=1; i<=N; i++) {
+            System.out.println("Digite informações do pedido: #" + i );
+            System.out.print("Nome do produto: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.print("Preço do produto: ");
+            double productPrice = sc.nextDouble();
+            System.out.print("Quantidade: ");
+            int quantity = sc.nextInt();
+
+            Product product = new Product(productName, productPrice);
+
+            OrderItem it = new OrderItem(quantity, productPrice, product);
+
+            order.addItem(it);
+        }
+
+        System.out.println();
+        System.out.println(order);
+
+        sc.close();
+    }
+}
---
+++ me.vitor.dalvi.exercice.9/src/entities/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package entities.enums;
+
+public enum OrderStatus {
+
+    AGUARDANDO,
+    PROCESSANDO,
+    ENVIADO,
+    CHEGOU;
+}
